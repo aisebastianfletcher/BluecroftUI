64a42d6dde743501ee498f9628ffd5d97627d242
aisebastianfletcher <aisebastianfletcher@gmail.com>
2025-12-02 00:35:02 -0100

    Refactor imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { LoanData, LoanType, CalculatedMetrics, RiskReport, ExitStrategy, AreaValuation, AuditLogEntry, ChatMessage, UploadedFile, CaseRecord, Applicant } from './types';
-import { parseDocument, generateRiskAnalysis, ... } from './services/geminiService';
+import { parseDocument, generateRiskAnalysis, checkAreaValuation, askUnderwriterAI } from './services/geminiService';
 import { generatePDFReport, generateRequestLetter, generateCalendarEvent } from './utils/pdfGenerator';
 import { LTVChart, RiskGauge } from './components/Charts';
 import React, { useState } from 'react';
