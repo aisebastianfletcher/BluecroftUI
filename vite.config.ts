0c58f90d384f73e4634f8dae69bfe6a898e8d741
aisebastianfletcher <aisebastianfletcher@gmail.com>
2025-12-01 23:12:29 -0100

    Refactor Vite config to include build and resolve options

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      // This allows your existing code using process.env.API_KEY to work
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
-    }
-  }
-})
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
