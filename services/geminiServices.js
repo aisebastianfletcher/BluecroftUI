0e7c31b9dbba9992a5fdddb7550d98f9a8edbc81
aisebastianfletcher <aisebastianfletcher@gmail.com>
2025-12-01 23:20:48 -0100

    Delete services/geminiServices.js

--- services/geminiServices.js
+++
@@ -1,4 +0,0 @@
-// Re-export shim to ensure bundlers that prefer resolving .js will find the module.
-// This file delegates to the TypeScript implementation geminiService.ts
-export * from './geminiService';
-export { default } from './geminiService';
